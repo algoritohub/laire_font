3bfb4d9a309e993887ba880ccad38481d080b329
jeferson-felipe-bezerra <115844982+jeferson-felipe-bezerra@users.noreply.github.com>
2023-04-18 02:42:55 -0300

    doenças

--- resources/views/respirasaude/doencas.blade.php
+++ resources/views/respirasaude/doencas.blade.php
@@ -42,7 +42,7 @@
             </div>
             {{--  --}}
             <div id="caixa_text_missao" class="w-[50%] pr-[5%] h-[700px] pt-[70px] inline-block float-left bg-[#5CD1FF]">
-                
+
                 {{-- ASMA --}}
                 @if (!empty($doenca) AND $doenca == "asma")
                 {{-- NOME DA DOENÇA --}}
@@ -68,29 +68,45 @@
                 @endif
 
                 {{-- PROXIMA DOENÇA CRÓNICA --}}
-                @if (!empty($doenca) AND $doenca == "dpoc")
+                @if (!empty($doenca) AND $doenca == "bronquiectasia")
                 {{-- NOME DA DOENÇA --}}
-                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">NOME DA DOENÇA AQUI!</p>
+                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">BRONQUIECTASIA</p>
                 {{--  --}}
-                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">DESCRIÇÃO DA DOENÇA AQUI</p>
+                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">Bronquiectasia é o aumento anormal, permanente e irreversível de estruturas pulmonares chamadas de brônquios e bronquíolos, causada por infecções recorrentes e inflamações. Alguns sintomas da doença são: tosse crônica, febre e grande produção de secreção, com presença de pus e com forte odor.</p>
                 @endif
 
                 {{-- DOENÇAS AGUDAS --}}
 
                 {{-- PRIMEIRA DOENÇA CRÓNICA --}}
-                @if (!empty($doenca) AND $doenca == "dpoc")
+                @if (!empty($doenca) AND $doenca == "fibrose_pulmonar")
                 {{-- NOME DA DOENÇA --}}
-                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">NOME DA DOENÇA AQUI!</p>
+                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">FIBROSE PULMONAR</p>
                 {{--  --}}
-                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">DESCRIÇÃO DA DOENÇA AQUI</p>
+                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">Doença crônica e progressiva em que os pulmões ficam danificados e com cicatrizes (fibrosados), causando espessamento e endurecimento das paredes pulmonares e, consequentemente, dificuldade para respirar.</p>
                 @endif
 
                 {{-- PRIMEIRA DOENÇA CRÓNICA --}}
-                @if (!empty($doenca) AND $doenca == "h1n1")
+                @if (!empty($doenca) AND $doenca == "sinusite")
                 {{-- NOME DA DOENÇA --}}
-                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">H1N1</p>
+                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">Sinusite</p>
                 {{--  --}}
-                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">A gripe H1N1 é uma infecção do trato respiratório, incluindo o nariz, garganta, brônquios e pulmão. É causado pelo vírus H1N1 e é altamente contagioso. Seus principais sintomas são febre, tosse, dor de garganta, mal estar geral, vômitos e dor de cabeça.
+                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">Inflamação dos seios paranasais (cavidades aéreas dentro dos ossos do crânio, situados ao redor do nariz). Os sintomas mais comuns são: produção de muco nasal espesso, nariz entupido e dor na face.
+                </p>
+                @endif
+                 {{-- PRIMEIRA DOENÇA CRÓNICA --}}
+                 @if (!empty($doenca) AND $doenca == "hipertensao_pulmonar")
+                 {{-- NOME DA DOENÇA --}}
+                 <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">HIPERTENSÃO PULMONAR </p>
+                 {{--  --}}
+                 <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">Doença progressiva, caracterizada pela elevação da pressão arterial pulmonar. Os sintomas mais comuns, são: falta de ar ao realizar atividades que demandam esforço, cansaço, fraqueza, desmaios, inchaço nos braços e mãos e inchaço abdominal.
+                 </p>
+                 @endif
+                  {{-- PRIMEIRA DOENÇA CRÓNICA --}}
+                @if (!empty($doenca) AND $doenca == "sindrome_pos_covid_19")
+                {{-- NOME DA DOENÇA --}}
+                <p id="text_missao_RS" class="text-[30px] leading-[30px] float-right text-right w-[500px] font-bold text-[#0C3D7F]">sindrome pós covid 19</p>
+                {{--  --}}
+                <p id="text_missao_RS" class="text-right mt-[40px] text-[{{ $numb_font }}px] float-right text-justify w-[550px] text-[#212121]">
                 </p>
                 @endif
             </div>
