4911eb473676f8062af6c383eec251ff7f3f8dd5
Algorito Hub <adm.firegreen@gmail.com>
2023-04-24 01:31:55 -0300

    atualização de layout teste projeto5

--- resources/views/respirasaude/projeto5.blade.php
+++ resources/views/respirasaude/projeto5.blade.php
@@ -33,8 +33,10 @@
                             <li class="mr-[2px] inline-block"><img id="libras" class="w-[40px] cursor-pointer" title="Ver libras" src="/img/libra.png"></li>
                         </ul>
                     </div>
-                    {{-- IMAGEM/VIDEO --}}
-                    <div id="imagem_countainer" class="w-[800px] bg-[yellow] h-[400px]"></div>
+                    {{-- IMAGEM --}}
+                    <div id="box_audio" class="w-[800px] bg-[yellow] h-[400px]"></div>
+                    {{-- VÍDEO --}}
+                    <div id="box_libra" style="display: none;" id="imagem_countainer" class="w-[800px] bg-[orange] h-[400px]"></div>
                 </div>
                 {{-- BASE LOGOS --}}
                 <div id="base_logos" class="w-[100%] h-[150px] inline-block">
@@ -59,5 +61,57 @@
         </div>
     </div>
 </div>
+
+{{-- BLOCO EXEMPLO 2 --}}
+<section class="w-[100%] mt-[-5px] inline-block">
+    {{--  --}}
+    <div id="container_bloco1" class="w-[100%] inline-block">
+        {{--  --}}
+        <div id="lado_a_bloco1" style="background: linear-gradient(90deg, rgba(238,238,238,1) 27%, rgba(250,250,250,1) 27%);" class="w-[50%] h-[630px] pl-[80px] float-left inline-block">
+            {{--  --}}
+            <div id="container_inner_bloco1" class="w-[100%] inline-block">
+                {{--  --}}
+                <div id="imagem_bloco1" class="w-[100%] h-[400px] bg-[purple] mt-[100px]"></div>
+            </div>
+        </div>
+        {{--  --}}
+        <div id="lado_b_bloco1" class="w-[50%] h-[630px] px-[80px] bg-[#fafafa] float-left inline-block">
+            {{--  --}}
+            <div id="container_inner_bloco1" class="w-[100%] inline-block">
+                {{--  --}}
+                <p id="titulo_bloco1" class="text-[25px] font-bold mb-[40px] mt-[100px] pl-[20px] border-l-[3px] border-l-[#00FF8C]">Vamos entender a importância da pesquisa</p>
+                <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">A realização deste estudo resultará em um avanço para o controle dos sintomas da asma em crianças, adolescentes e adultos brasileiros.</p>
+                <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">As atuais políticas públicas, que incluem a distribuição de medicamentos de baixo e alto custo, podem ser insuficientes devido à falta de mecanismos de avaliação e monitorização eficazes dos sintomas e riscos futuros decorrentes da asma.</p>
+                <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Vamos dar agora alguns exemplos de doenças respiratórias que são contagiosas, algumas delas você já deve conhecer, outras talvez não. Vamos lá!</p>
+                <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Os produtos desta proposta resultarão em um sistema de instrumentos que será de fácil implementação no cotidiano do paciente para facilitar o automanejo e a educação em asma.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+{{-- BLOCO EXEMPLO 3 --}}
+
+<section class="w-[100%] mt-[-5px] inline-block">
+    {{--  --}}
+    <div id="container_bloco2" class="w-[100%] inline-block">
+        {{--  --}}
+        <div id="lado_a_bloco2" class="w-[50%] h-[630px] px-[80px] inline-block float-left">
+            {{--  --}}
+            <div id="container_inner_bloco2" class="w-[100%] inline-block">
+                {{--  --}}
+                <p id="texto_titulo_bloco2" class="text-[25px] font-bold mb-[20px] mt-[100px] pl-[20px] border-l-[3px] border-l-[#00FF8C]"> Objetivo da pesquisa</p>
+                <p class="mb-[20px] text-[{{ $numb_font }}px] text-justify">desenvolvimento e validação de um sistema de avaliação e monitoramento do controle da asma e educação em saúde para crianças, adolescentes e adultos com asma (e-ASMA).</p>
+            </div> 
+        </div>
+        {{--  --}}
+        <div id="lado_b_bloco2" style="background: linear-gradient(90deg, rgba(255,255,255,1) 75%, rgba(238,238,238,1) 75%);" class="w-[50%] h-[630px] inline-block float-left pr-[80px]">
+            {{--  --}}
+            <div id="container_inner_bloco2" class="w-[100%] inline-block">
+                {{--  --}}
+                <div id="imagem_bloco2" class="w-[100%] h-[400px] bg-[purple] mt-[100px]"></div>
+            </div>
+        </div>
+    </div>
+</section>
 
 @endsection
