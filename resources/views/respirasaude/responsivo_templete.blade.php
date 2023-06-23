d9895cc99c07f4ab30cdacab865579a18d7009da
Gemenson França <adm.firegreen@gmail.com>
2023-06-23 00:45:35 -0300

    atualização 23/06/2023

--- resources/views/respirasaude/responsivo_templete.blade.php
+++ resources/views/respirasaude/responsivo_templete.blade.php
@@ -102,6 +102,26 @@
     li.sub{
         display: show;
     }
+    #section-master-prime{
+        background: linear-gradient(90deg, rgba(250,250,250,1) 26%, rgba(0,212,255,1) 68%);
+    }
+    .img-public{
+        margin-top: 80px;
+        border-radius: 20px;
+    }
+    #title-post{
+        margin-top: 80px;
+        font-size: 25px;
+        line-height: 25px;
+    }
+    #container-block{
+        width: 100%;
+        padding-bottom: 30px;
+        height: auto;
+    }
+    #img-pesquisador{
+        border-radius: 20px;
+    }
     /* CELULARES */
     @media all and (min-width:2px) and (max-width:798px) {
         #mixedSlider{
@@ -147,6 +167,61 @@
         #sub_cronicas{
             display: none;
         }
+        #block-prime-video{
+            width: 80%;
+            margin: 0px 10%;
+            padding-left: 0px;
+            height: auto;
+            display: inline-block;
+        }
+        #block-prime-text{
+            width: 80%;
+            margin: 0px 10%;
+            height: auto;
+            padding: 20px 0px;
+            display: inline-block;
+        }
+        #section-master-prime{
+            background: #5CD1FF;
+        }
+        #moldure-prime{
+            width: 100%;
+            height: 220px;
+        }
+        #container-block{
+            width: 100%;
+        }
+        .img-public{
+            margin-top: 40px;
+            border-radius: 20px;
+        }
+        #band_a_block{
+            width: 100%;
+            height: auto;
+            padding: 0px;
+        }
+        #band_b_block{
+            width: 100%;
+            padding: 0px;
+        }
+        #title-post{
+            margin-top: 30px;
+            font-size: 25px;
+            line-height: 25px;
+        }
+        #pesquisador-new{
+            width: 44%;
+            margin: 0px 3%;
+            height: auto;
+        }
+        #img-pesquisador{
+            height: 300px;
+            border-radius: 20px;
+        }
+        #rounded-img{
+            width: 120px;
+            height: 120px;
+        }
     }
     /* TABLETS */
     @media all and (min-width:800px) and (max-width:804px) {
@@ -187,6 +262,66 @@
         #sub_cronicas{
             display: none;
         }
+        #block-prime-video{
+            width: 90%;
+            margin: 0px 5%;
+            padding-left: 0px;
+            height: auto;
+            display: inline-block;
+        }
+        #block-prime-text{
+            width: 90%;
+            margin: 0px 5%;
+            height: auto;
+            padding: 20px 0px;
+            display: inline-block;
+        }
+        #section-master-prime{
+            background: #5CD1FF;
+        }
+        .img-public{
+            margin: 40px 0px;
+        }
+        #container_inner_bloco1{
+            width: 90%;
+            height: auto;
+            margin: 0px 5%;
+        }
+        #container_inner_bloco2{
+            width: 90%;
+            height: auto;
+            margin: 0px 5%;
+        }
+        #container-block{
+            width: 100%;
+        }
+        .img-public{
+            margin-top: 40px;
+            border-radius: 20px;
+        }
+        #band_a_block{
+            width: 100%;
+            height: auto;
+            padding: 0px;
+        }
+        #band_b_block{
+            width: 100%;
+            padding: 0px;
+        }
+        #title-post{
+            margin-top: 30px;
+            font-size: 25px;
+            line-height: 25px;
+        }
+        #pesquisador-new{
+            width: 30%;
+            margin: 0px 1.5%;
+            height: auto;
+        }
+        #img-pesquisador{
+            height: 310px;
+            border-radius: 20px;
+        }
     }
     /* RESPONSIVIDADE IPAD */
     @media all and (min-width:805px) and (max-width:815px) {
@@ -222,8 +357,90 @@
         #sub_cronicas{
             display: none;
         }
+        #moldure-prime{
+            width: 100%;
+            height: 360px;
+        }
+        #block-prime-video{
+            width: 90%;
+            margin: 0px 5%;
+            padding-left: 0px;
+            height: auto;
+            display: inline-block;
+        }
+        #block-prime-text{
+            width: 90%;
+            margin: 0px 5%;
+            height: auto;
+            padding: 20px 0px;
+            display: inline-block;
+        }
+        #section-master-prime{
+            background: #5CD1FF;
+        }
+        #container_inner_bloco1{
+            width: 90%;
+            height: auto;
+            margin: 0px 5%;
+        }
+        #container_inner_bloco2{
+            width: 90%;
+            height: auto;
+            margin: 0px 5%;
+        }
+        #container_inner_bloco1{
+            width: 90%;
+            height: auto;
+            margin: 0px 5%;
+        }
+        #lado_a_bloco1{
+            width: 100%;
+            height: auto;
+            padding: 0px;
+        }
+        #container-block{
+            width: 100%;
+        }
+        .img-public{
+            margin-top: 40px;
+            border-radius: 20px;
+        }
+        #band_a_block{
+            width: 100%;
+            height: auto;
+            padding: 0px;
+        }
+        #band_b_block{
+            width: 100%;
+            padding: 0px;
+        }
+        #title-post{
+            margin-top: 30px;
+            font-size: 25px;
+            line-height: 25px;
+        }
+        #pesquisador-new{
+            width: 30%;
+            margin: 0px 1.5%;
+            height: auto;
+        }
+        #img-pesquisador{
+            height: 310px;
+            border-radius: 20px;
+        }
     }
 </style>
+
+@php
+    // FUNÇÃO FONTE
+    $session_font = session('font');
+    if (isset($session_font) AND !empty($session_font)) {
+        $numb_font = session('font');
+    }
+    else{
+        $numb_font = 15;
+    }
+@endphp
 
 <body>
     <!-- HEADER  -->
@@ -238,7 +455,7 @@
                 <div id="header-right" class="w-[80%] h-[100px] float-left inline-block">
                     <div class="w-[100%] inline-block">
                         <!-- ACESSIBILITY -->
-                        <div id="shadow" class="w-[100%] px-[5%] float-left h-[50px] inline-block bg-[#212121]">
+                        <div id="shadow" class="w-[100%] px-[6%] float-left h-[50px] inline-block bg-[#212121]">
                             <div class="w-[100%] inline-block">
                                 <!-- BUTTONS -->
                                 <div id="acessibility-left" class="w-[70%] pr-[30px] inline-block float-left">
@@ -309,7 +526,7 @@
         </div>
     </header>
     {{-- SLIDE --}}
-    <div style="margin-top: 5px; z-index: -10;" class="slider">
+    <div style="margin-top: 5px; z-index: -10; display: none;" class="slider">
         {{--  --}}
         <div class="slides">
             <input type="radio" name="radio-btn" id="radio1">
@@ -342,6 +559,155 @@
         </div>
     </div>
 
+    {{-- NEW BLOCK PRIME --}}
+    <section id="section-master-prime" class="w-[100%] inline-block mt-[100px]">
+        <div class="w-[100%] inline-block">
+            <div class="w-[100%] inline-block">
+                {{-- BLOCK IMAGE PRIME --}}
+                <div id="block-prime-video" class="w-[50%] pl-[5%] float-left inline-block">
+                    {{-- BUTTON ACESSIBILITY --}}
+                    <div class="w-[100%] mt-[40px] inline-block">
+                        <ul>
+                            <li class="mr-[2px] inline-block"><img id="audios" class="w-[39px] cursor-pointer" title="Escute o texto" src="/img/audio.png"></li>
+                            <li class="mr-[2px] inline-block"><img id="libras" class="w-[40px] cursor-pointer" title="Ver libras" src="/img/libra.png"></li>
+                        </ul>
+                    </div>
+                    {{-- IMAGE/VIDEO --}}
+                    <div id="moldure-prime" class="w-[100%] inline-block bg-[purple] rounded-[20px] h-[400px]"></div>
+                    {{-- BASE LOGOS --}}
+                    <div id="base_logos" class="w-[80%] mt-[20px] h-[150px] inline-block">
+                        {{-- LOGOS --}}
+                        <div class="w-[40%] float-left inline-block">
+                            <img class="w-[80%]" src="/img/6.png" alt="CNPq">
+                        </div>
+                        {{--  --}}
+                        <div class="w-[60%] float-left inline-block">
+                            <img class="w-[100%]" src="/img/mcti.png" alt="Ministério Ciência e Tecnologia">
+                        </div>
+                    </div>
+                </div>
+                {{-- BLOCK TEXT PRIME --}}
+                <div id="block-prime-text" class="w-[45%] float-left inline-block pl-[80px] pb-[20px] pt-[80px] text-justify">
+                    <p class="text-[30px] leading-[30px] float-right text-right font-bold text-[#080E45]"> Soluções para aumentar a aceitabilidade, adesão e cumprimento das medidas de prevenção e controle da covid-19 na população.</p>
+                    <p class="text-right mt-[40px] float-right text-justify text-[#080E45] text-[{{ $numb_font }}px]">Esta pesquisa está sendo financiada pelo Conselho Nacional de Desenvolvimento Científico e Tecnológico (CNPq),</p>
+                    <p class="mt-[20px] text-right float-right text-justify text-[#080E45] text-[{{ $numb_font }}px]">Ministério da Ciência, Tecnologia, Inovações e Comunicações (MCTIC) e Ministério da Saúde (MS). O seu objetivo é propor soluções para que a população seja capaz de aumentar a sua adesão às medidas de prevenção e controle da COVID-19 e outras doenças respiratórias contagiosas. Para isso, realizaremos uma importante e extensa revisão sistemática para compreender as barreiras e facilitadores da adesão às medidas de prevenção e controle a nível mundial. Posteriormente, a partir do desenvolvimento, validação a aplicação do questionário ADHERE no território nacional. </p>
+                    <p class="mt-[20px] text-right float-right text-justify text-[#080E45] text-[{{ $numb_font }}px]">Ministério da Ciência, Tecnologia, Inovações e Comunicações (MCTIC) e Ministério da Saúde (MS). O seu objetivo é propor soluções para que a população seja capaz de aumentar a sua adesão às medidas de prevenção e controle da COVID-19 e outras doenças respiratórias contagiosas. Para isso, realizaremos uma importante e extensa revisão sistemática para compreender as barreiras e facilitadores da adesão às medidas de prevenção e controle a nível mundial. Posteriormente, a partir do desenvolvimento, validação a aplicação do questionário ADHERE no território nacional. </p>
+                    {{-- AUDIO --}}
+                    <div class="w-[100%] inline-block">
+                        {{--  --}}
+                        <div id="audio_libra" style="display: none;" class="inline-block float-right mt-[20px]">
+                            {{--  --}}
+                            <audio autoplay="autoplay" controls="controls">
+                                <source src="sua_musica.mp3" type="audio/mp3" />
+                                seu navegador não suporta HTML5
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{-- BLOCO 1 --}}
+    <section class="w-[100%] mt-[-5px] bg-[#f1f1f1] inline-block">
+        {{--  --}}
+        <div id="container-block" class="w-[100%] inline-block">
+            {{--  --}}
+            <div id="band_a_block" class="w-[50%] pl-[80px] float-left inline-block">
+                {{--  --}}
+                <div id="container_inner_bloco1" class="w-[100%] inline-block">
+                    {{--  --}}
+                    <img class="img-public" src="/img/Pesquisa 2/Pesquisa 2 - imagem 1.png">
+                </div>
+            </div>
+            {{--  --}}
+            <div id="band_b_block" class="w-[50%] px-[80px] float-left inline-block">
+                <div id="importacncia_projeto2"></div>
+                {{--  --}}
+                <div id="container_inner_bloco1" class="w-[100%] inline-block">
+                    {{--  --}}
+                    <p id="title-post" class="text-[25px] font-bold mb-[40px] mt-[100px] pl-[20px] border-l-[3px] border-l-[#00FF8C]">Vamos entender a importância da pesquisa</p>
+                    <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Algumas doenças respiratórias infecciosas matam milhares de brasileiros todos os anos, pois elas são contagiosas e passam de uma pessoa para outra muito facilmente através da respiração, tosse, espirros e mãos contaminadas.</b>.</p>
+                    <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Então, saber um pouco mais sobre essas doenças é muito importante. Precisamos entender como elas são transmitidas, quais são os seus sintomas e principalmente, como podemos nos proteger do contágio. Então, pense conosco... se nós conhecermos melhor essas doenças, e como nós podemos nos proteger delas, será possível diminuir a <b>mortalidade</b> no nosso país!</p>
+                    <p id="text_projeto3" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Vamos dar agora alguns exemplos de doenças respiratórias que são contagiosas, algumas delas você já deve conhecer, outras talvez não. Vamos lá!</p>
+                    <p id="text_projeto2" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Você já ouviu falar em <b>pneumonia</b>, <b>tuberculose</b>, <b>H1N1</b>, <b>bronquiolite</b>, <b>influenza</b>, <b>coqueluche</b>, <b>COVID-19</b></p>
+                    <p id="text_projeto2" class="mb-[20px] text-justify text-[{{ $numb_font }}px]">Vamos compartilhar aqui de uma forma muito simples, como esta pesquisa está acontecendo e os seus resultados. Ela está sendo realizada com o apoio do Ministério da Saúde, CNPq. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{-- BLOCO 2 --}}
+    <section class="w-[100%] mt-[-5px] bg-[#fafafa] inline-block">
+        {{--  --}}
+        <div id="container-block" class="w-[100%] inline-block">
+            {{--  --}}
+            <div id="band_a_block" class="w-[50%] px-[80px] inline-block float-left">
+                {{--  --}}
+                <div id="container_inner_bloco2" class="w-[100%] inline-block">
+                    {{--  --}}
+                    <p id="title-post" class="text-[25px] font-bold mb-[20px] mt-[90px] pl-[20px] mb-[20px] border-l-[3px] border-l-[#00FF8C]">Objetivo da Pesquisa </p>
+    
+                    <p id="metodo" class="mb-[20px] text-[{{ $numb_font }}px] text-justify">Propor soluções para que a população seja capaz de aumentar a sua adesão às medidas de prevenção </b>.</p>
+                    <p class="mb-[20px] text-[{{ $numb_font }}px] text-justify">e controle da COVID-19 e outras doenças respiratórias contagiosas. </p>
+                    <p class="mt-[20px] text-right float-right text-justify text-[#080E45] text-[{{ $numb_font }}px]">Ministério da Ciência, Tecnologia, Inovações e Comunicações (MCTIC) e Ministério da Saúde (MS). O seu objetivo é propor soluções para que a população seja capaz de aumentar a sua adesão às medidas de prevenção e controle da COVID-19 e outras doenças respiratórias contagiosas. Para isso, realizaremos uma importante e extensa revisão sistemática para compreender as barreiras e facilitadores da adesão às medidas de prevenção e controle a nível mundial. Posteriormente, a partir do desenvolvimento, validação a aplicação do questionário ADHERE no território nacional. </p>
+                    <p class="mt-[20px] text-right float-right text-justify text-[#080E45] text-[{{ $numb_font }}px]">Ministério da Ciência, Tecnologia, Inovações e Comunicações (MCTIC) e Ministério da Saúde (MS). O seu objetivo é propor soluções para que a população seja capaz de aumentar a sua adesão às medidas de prevenção e controle da COVID-19 e outras doenças respiratórias contagiosas. Para isso, realizaremos uma importante e extensa revisão sistemática para compreender as barreiras e facilitadores da adesão às medidas de prevenção e controle a nível mundial. Posteriormente, a partir do desenvolvimento, validação a aplicação do questionário ADHERE no território nacional. </p>
+                </div>
+            </div>
+            {{--  --}}
+            <div id="band_b_block" class="w-[50%] inline-block float-left pr-[80px]">
+                {{--  --}}
+                <div id="container_inner_bloco2" class="w-[100%] inline-block">
+                    {{--  --}}
+                    <img class="img-public" src="/img/Pesquisa 1 - imagem 5_20230510_073059_0004.png">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{-- PESQUISADORES --}}
+    <section class="w-[100%] inline-block">
+        <div class="w-[94%] mx-[3%] my-[40px] inline-block inline-block">
+            <center><p class="font-bold text-[25px]">Pesquisadores</p></center>
+        </div>
+        {{-- LISTA DE PESQUISADORES --}}
+        <div class="w-[94%] mx-[3%] mt-[60px] inline-block">
+            @if($pesquisadores)
+                @foreach ($pesquisadores as $pessoa)
+
+                @php
+                $id_pesqs = $pessoa->id;
+                $pesquisa = Illuminate\Support\Facades\DB::select("SELECT * FROM conect_pesquisas WHERE pesquisa = 1 AND pesquisador = '$id_pesqs'");
+                @endphp
+
+                @if($pesquisa)
+                {{-- CARD PESQUISADOR --}}
+                <div id="pesquisador-new" class="w-[20%] mx-[2.5%] float-left">
+                    {{--  --}}
+                    <div id="img-pesquisador" class="w-[100%] bg-[#fafafa] shadow-lg h-[350px] inline-block border-t-[5px] border-t-[orange]">
+                        {{--  --}}
+                        <div id="rounded-img" style="background: url('/img/pesquisadores/{{ $pessoa->imagem }}'); background-size: 100%;" class="w-[150px] h-[150px] rounded-[100px] mt-[50px] bg-[#FFC122] mx-auto transform hover:scale-110 transition duration-300 border-[1px]"></div>
+                        {{--  --}}
+                        <div class="w-[90%] mx-[5%] mt-[20px] inline-block">
+                            {{--  --}}
+                            <p class="font-bold text-center text-[15px]">{{ $pessoa->nome }}</p>
+                            {{--  --}}
+                            <p class="text-[#212121] text-center text-[16px]">{{ $pessoa->referencia }}</p>
+                        </div>
+                    </div>
+                    {{--  --}}
+                    <div class="w-[100%] inline-block h-[150px]">
+                        {{--  --}}
+                        <a href="{{ route('pesquisadores_projeto_1', ['id' => $pessoa->id]) }}"><p class="text-center uppercase text-[15px] mt-[25px] font-bold">ver bio ➜</p></a>
+                    </div>
+                </div>
+                @endif
+                @endforeach
+            @else
+            <p class="text-center text-[20px] font-bold mt-[30px]">Sem pesquisadores para esse projeto!</p>
+            @endif
+        </section>
+    </section>
 
     <!-- BODY  -->
     <section class="w-[100%] h-[1000px]"></section>
